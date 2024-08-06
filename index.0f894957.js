33dd6dc3da2f9a0152d3449f627d437aad598314
Ivan Mondok <ivanmondok8@gmail.com>
2024-08-06 19:30:47 +0300

    make build

---
+++ index.0f894957.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t)if(t.hasOwnProperty(a)){var l=document.createElement("li");l.textContent=a,r.appendChild(l),Object.keys(t[a]).length&&e(l,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.0f894957.js.map
